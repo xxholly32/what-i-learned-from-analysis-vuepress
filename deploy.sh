d6c773b5a6745fb8b3a8506ad658357607845937
相霄 <xiangxiao@xiangxiaodeMacBook-Pro.local>
2019-08-23 02:52:39 +0800

    fix: fix build bug

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ git init
 git add -A
 git commit -m 'deploy: 发布到 gh-pages'
 
-git push -f https://github.com/xxholly32/Blog.git master:gh-pages
+git push -f https://github.com/xxholly32/what-i-learned-from-analysis-vuepress.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   chainWebpack: config => {
-    config.module.rule('md')
+    config.module
+      .rule("md")
       .test(/\.md/)
-      .use('file-loader')
-      .loader('file-loader')
-      .end()
-  }
-}
+      .use("file-loader")
+      .loader("file-loader")
+      .end();
+  },
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/what-i-learned-from-analysis-vuepress/"
+      : "/"
+};
